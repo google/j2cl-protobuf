3ba64d80ce598f2f59ac7afbd5b6676ccc385f52
Goktug Gokdogan <goktug@google.com>
2022-08-10 11:45:11 -0700

    Remove JsType annotation which can unnecessarily add to code size with bridges and make the proto Java enums accessible from JS.
    PiperOrigin-RevId: 466748192

--- java/com/google/protobuf/contrib/j2cl/runtime/super/com/google/protobuf/ProtocolMessageEnum.java
+++ java/com/google/protobuf/contrib/j2cl/runtime/super/com/google/protobuf/ProtocolMessageEnum.java
@@ -13,10 +13,7 @@
  */
 package com.google.protobuf;
 
-import jsinterop.annotations.JsType;
-
 /** J2CL emulation of ProtocolMessageEnum. */
-@JsType
 public interface ProtocolMessageEnum {
   int getNumber();
 }
